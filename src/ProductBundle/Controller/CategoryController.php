777b8eb9d2fe839b181a36686671c271ecefcbc9
nesamouzehkesh <n.mouzehkesh@gmail.com>
2016-08-17 13:19:57 +1000

    category Api not working

--- src/ProductBundle/Controller/CategoryController.php
+++ src/ProductBundle/Controller/CategoryController.php
@@ -2,23 +2,32 @@
 
 namespace ProductBundle\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use FOS\RestBundle\Controller\Annotations;
+use FOS\RestBundle\Controller\Annotations\Get;
 use Nelmio\ApiDocBundle\Annotation\ApiDoc;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\HttpFoundation\Response;
 use FOS\RestBundle\Controller\FOSRestController;
+use ProductBundle\Entity\ProductCategory;
+
 
 class CategoryController extends FOSRestController
 {
     /**
     * @ApiDoc()
+    * 
+    * @Get("/categories", name="api_admin_get_categories", options={ "method_prefix" = false })
     */
-    public function getCategoriesAction()
+    public function getCategoriesAction(Request $request)
     {
-        $em = $this->getDoctrine()->getManager();
-        $categories = $em
+        $id = $request->query->get('id', null);
+        
+        $categories = $this
+            ->getDoctrine()
+            ->getEntityManager()
             ->getRepository('ProductBundle:ProductCategory')
             ->getCategories();
         
